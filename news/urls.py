a286aa7468067cc2dbbd84e35fffec20037d6ef7
kaplunvv <kaplunvv@ekis.ru>
2017-12-25 09:19:41 +0300

    urls fix

--- news/urls.py
+++ news/urls.py
@@ -5,5 +5,5 @@ from .admin import news_admin
 
 urlpatterns = [
     url(r'^$', NewsList.as_view(), name="news-list"),
-    url(r'^(?P<slug>\d+)/$', NewsDetailView.as_view(), name="news-detail"),
+    url(r'^(?P<pk>\d+)$', NewsDetailView.as_view(), name="news-detail"),
 ]
