f6975cdb96021467c943e3303baa93c80053e7b1
kaplunvv <kaplunvv@ekis.ru>
2017-12-25 08:55:57 +0300

    encoding fix

--- news/models.py
+++ news/models.py
@@ -1,3 +1,4 @@
+# encoding: utf-8
 from django.db import models
 from django.utils import timezone
 import os
@@ -15,17 +16,17 @@ class News(models.Model):
     def upload_image_to(instance, filename):
         return os.path.join('news', str(instance.pk or 'new'), filename)
 
-    title = models.CharField(max_length=500, verbose_name="Заголовок")
-    created_on = models.DateTimeField(default=timezone.now, verbose_name='Время создания')
-    published = models.BooleanField(blank=True, default=False, verbose_name="Опубликован?")
-    published_on = models.DateTimeField(blank=True, null=True, verbose_name="Когда опубликовать")
-    content = RichTextField(blank=True, max_length=5000, verbose_name="Текст новости", config_name='news_editor')
+    title = models.CharField(max_length=500, verbose_name=u"Заголовок")
+    created_on = models.DateTimeField(default=timezone.now, verbose_name=u'Время создания')
+    published = models.BooleanField(blank=True, default=False, verbose_name=u"Опубликован?")
+    published_on = models.DateTimeField(blank=True, null=True, verbose_name=u"Когда опубликовать")
+    content = RichTextField(blank=True, max_length=5000, verbose_name=u"Текст новости", config_name='news_editor')
     image = models.ImageField(blank=True, verbose_name="Изображение", upload_to=upload_image_to)
 
     def __str__(self):
-        ttl = "%s (%s)" % (self.title, self.created_on.strftime("%d.%m.%y %H:%M"))
+        ttl = u"%s (%s)" % (self.title, self.created_on.strftime("%d.%m.%y %H:%M"))
         if not self.published:
-            ttl = "<span style='color:#cacaca;'>%s</span>" % ttl
+            ttl = u"<span style='color:#cacaca;'>%s</span>" % ttl
         return mark_safe(ttl)
 
     def save(self, *args, **kwargs):
@@ -34,5 +35,5 @@ class News(models.Model):
         if not self.title:
             from django.utils import formats
             self.title = formats.date_format(self.published_on, 'd E Y') if self.published_on \
-                else formats.date_format(self.created_on, 'Создан d E Y')
+                else formats.date_format(self.created_on, u'Создан d E Y')
         super(News, self).save(*args, **kwargs)
